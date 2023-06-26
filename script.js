f1bb28d5b257ddb564006b4af0e8f0f8e289edd5
TechKing <Obafemielijahsunday@gmail.com>
2023-06-26 23:08:37 +0100

    js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var settingMenu = document.querySelector(".setting-menu");
+var settingmenu = document.querySelector(".setting-menu");
 
 
 function settingsMenuToggle(){
-    settingMenu.classList.toggle("setting-menu-height");
+    settingmenu.classList.toggle("setting-menu-height");
 }
