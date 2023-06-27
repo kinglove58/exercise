2775e1ab334043a8d44e1b32e8f0663d3cfcd575
TechKing <Obafemielijahsunday@gmail.com>
2023-06-27 22:44:48 +0100

    commit js

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-var settingmenu = document.querySelector(".setting-menu");
+var settingMenu = document.querySelector(".setting-menu");
+var darkBtn = document.getElementById("dark-btn")
 
+function settingsMenuToggle() {
+  settingMenu.classList.toggle("setting-menu-height");
+}
 
-function settingsMenuToggle(){
-    settingmenu.classList.toggle("setting-menu-height");
+darkBtn.onclick = function(){
+  darkBtn.classList.toggle("dark-btn-on");
 }
